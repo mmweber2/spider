a65ff084efd844f25aef8d20c9bd7d93f7e42b8a
Mary Ellen Weber <mmweber2@gmail.com>
2016-09-10 07:08:28 -0500

    Add unicode encoding to spider.

--- spider.py
+++ spider.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
 import urllib2
 from bs4 import BeautifulSoup
 
 html = urllib2.urlopen('http://www.pokeai.net/toz/dialga').read()
-soup = BeautifulSoup(html, 'lxml')
-print soup.get_text()
+soup = BeautifulSoup(html, 'lxml', from_encoding="utf-8")
+dialga = u"ディアルガ"
+print dialga in soup.get_text()
