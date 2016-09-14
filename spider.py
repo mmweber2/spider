b806bd67c9555256e48b55c007c1da495037c1e7
Mary Ellen Weber <mmweber2@gmail.com>
2016-09-14 16:05:26 -0500

    Check for JP in a page's meta contents.

--- spider.py
+++ spider.py
@@ -2,15 +2,22 @@
 # -*- coding: UTF-8 -*-
 from __future__ import unicode_literals
 import urllib2
+import MeCab
 from bs4 import BeautifulSoup
 
-def _contains_jp(text):
-    """Returns True iff text contains Japanese hiragana/katakana characters."""
-    for char in text:
-        if (u'ぁ' <= char <= u'ん') or (u'ァ' <= char <= u'・'):
-            return True
+def _contains_jp(texts):
+    """Returns True iff texts contains Japanese hiragana/katakana characters."""
+    for text in texts:
+        for char in text:
+            if (u'ぁ' <= char <= u'ん') or (u'ァ' <= char <= u'・'):
+                return True
+    return False
 
-html = urllib2.urlopen('http://www.pokeai.net/toz/dialga').read()
+html = urllib2.urlopen('http://www.yahoo.co.jp').read()
 soup = BeautifulSoup(html, 'lxml', from_encoding="utf-8")
-dialga = u"ディアルガ"
-print _contains_jp(soup.get_text())
+contents = [x.get('content') for x in soup.find_all('meta')]
+print _contains_jp(contents)
+
+#mecab = MeCab.Tagger()
+#words = mecab.parseToNode(text)
+#print mecab.parse(soup.get_text()[:50])
