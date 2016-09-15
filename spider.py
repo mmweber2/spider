f6aa06d2f6c93080e1beb5a8340720f5ec816934
Mary Ellen Weber <mmweber2@gmail.com>
2016-09-15 16:00:08 -0500

    Create Page object and __str__ for Page.

--- spider.py
+++ spider.py
@@ -3,7 +3,40 @@
 from __future__ import unicode_literals
 import urllib2
 import MeCab
+import sys
 from bs4 import BeautifulSoup
+
+class Page(object):
+    """Represents a single web page."""
+
+    def __init__(self, url, text, links):
+        """Creates a new Page.
+
+        text and links are determined by parsing the page with Beautiful Soup 4.
+
+        Args:
+            url: string, the absolute URL at which this page was found.
+
+            text: string, the text of this page. May contain some html, xml,
+                CSS code, or other formatting, but is intended to be text only.
+
+            links: list, a list of absolute URL links found on the page.
+        """
+        self.url = url
+        self.text = text
+        self.links = links
+
+    def __str__(self):
+        return u"URL: {}\nFirst 100 characters of text: {}\n\nLinks: {}".format(
+                self.url, self.text.strip()[:100], self.links)
+
+def _parse_page(url):
+    """Creates a new Page by retrieving and parsing the given url."""
+    html = urllib2.urlopen(url).read()
+    soup = BeautifulSoup(html, 'lxml', from_encoding="utf-8")
+    #contents = [x.get('content') for x in soup.find_all('meta')]
+    links = [link.get('href') for link in soup.find_all('a')]
+    return Page(url, soup.get_text(), links)
 
 def _contains_jp(texts):
     """Returns True iff texts contains Japanese hiragana/katakana characters."""
@@ -13,11 +46,12 @@ def _contains_jp(texts):
                 return True
     return False
 
-html = urllib2.urlopen('http://www.yahoo.co.jp').read()
-soup = BeautifulSoup(html, 'lxml', from_encoding="utf-8")
-contents = [x.get('content') for x in soup.find_all('meta')]
-print _contains_jp(contents)
+page = _parse_page('http://www.yahoo.co.jp')
+print page.__str__()
+#print _contains_jp(page.text)
 
-#mecab = MeCab.Tagger()
-#words = mecab.parseToNode(text)
-#print mecab.parse(soup.get_text()[:50])
+
+# TODO: Get MeCab parsing working within program
+#mecab = MeCab.Tagger(" ".join(sys.argv))
+#words = mecab.parse(text)
+#print words
