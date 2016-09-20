48b04a0d3977bdc9e414046264c153b86da80b39
Mary Ellen Weber <mmweber2@gmail.com>
2016-09-20 17:57:13 -0500

    Add more tests for Spider.

--- test_spider.py
+++ test_spider.py
@@ -1,6 +1,28 @@
 from spider import Spider
+from nose.tools import assert_equals
+from nose.tools import assert_raises
 
-def test_no_links():
+def test_no_links_http():
     start = "http://www.pokeai.net"
-    spider = Spider(start)
-    print spider.crawl()
+    result = Spider(start).crawl()
+    assert_equals(result[0].links, [])
+    assert_equals(len(result), 1)
+
+def test_no_links_https():
+    start = "https://www.google.com"
+    result = Spider(start).crawl(1)
+    assert start in [x.url for x in result]
+    assert_equals(len(result), 2)
+
+def test_many_links_choose_one():
+    start = "http://www.yahoo.com"
+    result = Spider(start).crawl(1)
+    assert start in [x.url for x in result]
+    assert_equals(len(result), 2)
+
+def test_invalid_url_no_http():
+    start = "www.yahoo.com"
+    assert_raises(ValueError, Spider, start)
+
+def test_invalid_url_number():
+    assert_raises(AttributeError, Spider, 3)
