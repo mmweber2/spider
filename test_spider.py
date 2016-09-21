cdd62864181a782a92cb6742a1195556e5aab83b
Mary Ellen Weber <mmweber2@gmail.com>
2016-09-21 08:37:22 -0500

    Add test for crawl with max_links = 0.

--- test_spider.py
+++ test_spider.py
@@ -2,27 +2,33 @@ from spider import Spider
 from nose.tools import assert_equals
 from nose.tools import assert_raises
 
-def test_no_links_http():
+def test_crawl_no_links_http():
     start = "http://www.pokeai.net"
     result = Spider(start).crawl()
     assert_equals(result[0].links, [])
     assert_equals(len(result), 1)
 
-def test_no_links_https():
+def test_crawl_many_links_https():
     start = "https://www.google.com"
     result = Spider(start).crawl(1)
     assert start in [x.url for x in result]
     assert_equals(len(result), 2)
 
-def test_many_links_choose_one():
+def test_crawl_many_links_http():
     start = "http://www.yahoo.com"
     result = Spider(start).crawl(1)
     assert start in [x.url for x in result]
     assert_equals(len(result), 2)
 
-def test_invalid_url_no_http():
+def test_crawl_max_links_zero():
+    start = "http://www.yahoo.com"
+    result = Spider(start).crawl(0)
+    assert_equals(result[0].url, start)
+    assert_equals(len(result), 1)
+
+def test_constructor_invalid_url_no_http():
     start = "www.yahoo.com"
     assert_raises(ValueError, Spider, start)
 
-def test_invalid_url_number():
+def test_constructor_invalid_url_number():
     assert_raises(AttributeError, Spider, 3)
